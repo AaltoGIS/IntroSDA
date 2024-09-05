ae296d4997373b769f82a103de2a89d20dac98c1
Henrikki Tenkanen <henrikki.tenkanen@aalto.fi>
2024-09-05 14:36:23 +0300

    Use smaller logo

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = 'img/Aalto_logo.png'
+html_logo = 'img/slack-icon.png'
 html_title = ""
 
 # Custom CSS
